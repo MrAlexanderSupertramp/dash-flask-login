d7d48d13d35ed47d1f7fadbdb0fd64a66e00c12d
Jiril Zala <zjiril@gmail.com>
2022-05-27 19:43:52 +0530

    heroku correction - 2

--- app.py
+++ app.py
@@ -62,8 +62,6 @@ app = flask_app()
 
 
 if __name__ == "__main__":
-
-    port = int(os.getenv('PORT'))
     # app.run(debug=True)
     app.run()
     # app.run(host="0.0.0.0", port=5000, threaded=True)
