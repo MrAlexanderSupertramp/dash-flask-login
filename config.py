ac5a6f0cfcdd4d5d9e1fdf1fb397d0085870bb3f
Jiril Zala <zjiril@gmail.com>
2022-05-28 10:06:44 +0530

    initial security enhancements (config)

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import datetime
 
 class Config(object):
     CSRF_ENABLED = True
-    SECRET_KEY = '57e19ea558d4967a552d03deece34a70'
+    SECRET_KEY = '57e19ea558d4967a552d03deece34a7000'
     SQLALCHEMY_DATABASE_URI="sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_SQLALCHEMY_TABLE = 'sessions'
     SESSION_TIMEOUT = 60*60*24*30   # 30 days : seconds*minutes*hours*days
     TOKEN_TIMEOUT = 60*30          # 30 minutes : seconds*minutes
     TIMEZONE = 'Asia/Kolkata'
-    MAIL_SERVER = "mail.krioskcreata.com"  
-    MAIL_PORT = 2525
-    MAIL_USERNAME = "mailer@krioskcreata.com" 
-    MAIL_PASSWORD = "Krioskcreata@18"
-    MAIL_USE_TLS = True  
-    MAIL_USE_SSL = False
+    MAIL_SERVER = ""     #string
+    MAIL_PORT = 2525     #integer
+    MAIL_USERNAME = ""   #string
+    MAIL_PASSWORD = ""   #string
+    MAIL_USE_TLS = False #boolean
+    MAIL_USE_SSL = False #boolean
 
     # TESTING = False
     # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
